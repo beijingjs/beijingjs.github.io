d4cebbd6b9a3ae35936040b08604ee215521acb0
eyea <deyesea@gmail.com>
2017-02-22 22:49:52 +0800

    init：英雄榜初始化；

--- source/categories/index.md
+++ source/categories/index.md
@@ -3,3 +3,4 @@ title: categories
 date: 2017-02-15 22:38:36
 type: "categories"
 ---
+dfdff
---
+++ themes/free2mind/source/js/hero_brand.js
@@ -0,0 +1,36 @@
+/**
+ * @author: eyea
+ * @description:
+ * @Date: 2017/2/2 22:08
+ */
+;(function($){
+  $.get('/data/beijing.json',function(res){
+
+    for(var i=0,len = res.length;i<len;i++){
+      var html;
+      html = "<div class='col-md-4 text-center'>"
+                 + "<img class='hero_avatar' src= " + res[i]['avatar'] + " alt='avatar'>"
+                 + "<h3>" + res[i]['username'] + "</h3>"
+                 + "<p class='hero_links'>"
+                 +    "<a href= " + res[i]['blog']+  "target='_blank'>博客</a>"
+                 +    "<a href= " + res[i]['github']+ "target='_blank'>站点</a>"
+                 + "</p>"
+                 + "<p class='hero_intro'> " + res[i]['word']+ "</p>";
+              +"</div>"
+
+        $('.hero_container')[0].innerHTML += html;
+        html = null;
+    };
+
+  })
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
