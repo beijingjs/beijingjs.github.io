7e7fa51dfab0e985f62b29f90d15def04001b3f9
eyea <deyesea@gmail.com>
2017-02-23 00:03:30 +0800

    英雄榜：1.解决链接bug；2.更新榜单；

--- themes/free2mind/source/js/hero_brand.js
+++ themes/free2mind/source/js/hero_brand.js
@@ -12,8 +12,8 @@
                  + "<img class='hero_avatar' src= " + res[i]['avatar'] + " alt='avatar'>"
                  + "<h3>" + res[i]['username'] + "</h3>"
                  + "<p class='hero_links'>"
-                 +    "<a href= " + res[i]['blog']+  "target='_blank'>博客</a>"
-                 +    "<a href= " + res[i]['github']+ "target='_blank'>站点</a>"
+                 +    "<a href= " + res[i]['blog']+  " target='_blank'>博客</a>"
+                 +    "<a href= " + res[i]['github']+ " target='_blank'>站点</a>"
                  + "</p>"
                  + "<p class='hero_intro'> " + res[i]['word']+ "</p>";
               +"</div>"
